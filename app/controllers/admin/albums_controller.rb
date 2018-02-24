9f43e8b95d75510051073349e783e61818d3b3d6
Eric Oligney <EOengineer@gmail.com>
2018-02-24 12:18:44 -0500

    reusable admin table helpers

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -2,10 +2,10 @@ class Admin::AlbumsController < Admin::BaseController
 
 	def index
 		@resource_name = "Albums"
-		@columns = get_columns
-		@albums  = Album.order(title: :asc)
-		@current_page = params[:page] || 1
-		@per_page = params[:per_page] || 10
+		@columns 			 = get_columns
+		@albums  			 = Album.order(title: :asc)
+		@current_page  = params[:page] || 1
+		@per_page 		 = params[:per_page] || 10
 		add_breadcrumb "Albums", :admin_albums
 	end
 
@@ -13,12 +13,14 @@ class Admin::AlbumsController < Admin::BaseController
 	private
 
 	def get_columns
-		[ { field: 'id', label: 'ID', type: 'int' },
+		[
+			{ field: 'id', label: 'ID', type: 'integer' },
 			{ field: 'title', label: 'Title', type: 'string' },
 			{ field: 'release_date', label: 'Release Date', type: 'date' },
 			{ field: 'artist', label: 'Artist', association_field: 'title', type: 'string' },
 			{ field: 'label', label: 'Label', association_field: 'title', type: 'string' },
-			{ field: 'available', label: 'Available', type: 'boolean' }]
+			{ field: 'available', label: 'Available', type: 'boolean' }
+		]
 	end
 
 end
