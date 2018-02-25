a6b4d09e47537d808ee6d2d6a2787dfd22e009d9
Eric Oligney <EOengineer@gmail.com>
2018-02-25 15:27:41 -0500

    column sorting for admin tables

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -1,22 +1,25 @@
 class Admin::AlbumsController < Admin::BaseController
 
 	def index
-		@resource_name = "Albums"
-		@columns 			 = get_columns
+		@resource_name  	 = "Albums"
+		@columns 			  	 = get_columns
+		@current_page   	 = params[:current_page].try(:to_i) || 1
+		@per_page 		  	 = params[:per_page].try(:to_i) || 10
+		@sort_field		  	 = params[:sort_field] || 'id'
+		joined_sort_field  = handle_associated_sort
+		@sort_direction    = params[:sort_direction] || 'asc'
+
+		@albums  			     = Album.all
+		@total_count	 		 = @albums.count
+
+		@query             = @albums.includes(:artist, :label).order("#{joined_sort_field} #{@sort_direction}").limit(@per_page).offset((@current_page - 1) * @per_page)
 		
-		@current_page  = params[:current_page].to_i || 1
-		@per_page 		 = params[:per_page].to_i || 10
-
-		@albums  			 = Album.all
-		@total_count	 = @albums.count
-
-		@query         = @albums.limit(@per_page).offset((@current_page - 1) * @per_page)
-		
 
 
-		@pages 				 = (@total_count / @per_page).floor 
+		@pages 				     = (@total_count / @per_page).floor 
 		add_breadcrumb "Albums", :admin_albums
 	end
+
 
 
 	private
@@ -30,6 +33,12 @@ class Admin::AlbumsController < Admin::BaseController
 			{ field: 'label', label: 'Label', association_field: 'title', type: 'string' },
 			{ field: 'available', label: 'Available', type: 'boolean' }
 		]
+	end
+
+
+	def handle_associated_sort
+		sort_mapping 			= @columns.select { |col| col[:field] == @sort_field }.try(:first)
+		sort_field_string = sort_mapping[:association_field] ? "#{@sort_field.pluralize}.#{sort_mapping[:association_field]}" : @sort_field
 	end
 
 end
